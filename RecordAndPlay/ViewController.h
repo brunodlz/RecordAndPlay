e11c8d6c41430317b875e23b774456e77282f80d
Bruno da Luz <brunolabx@gmail.com>
2014-10-28 20:45:53 -0200

    Initial Commit

---
+++ RecordAndPlay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RecordAndPlay
+//
+//  Created by Bruno on 10/28/14.
+//  Copyright (c) 2014 Bruno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
